f581972b7d6e1e7f6bea3cb74f327c0de2a626da
Duohong <duohongopen@gmail.com>
2018-07-07 20:16:33 -0400

    test instance method

--- Production/Calculator.cs
+++ Production/Calculator.cs
@@ -4,7 +4,30 @@ namespace Production
 {
     public class Calculator
     {
-        public int Add(int a, int b)
+        private int mMemory;
+
+        public Calculator()
+            : this (0)
+        { }
+
+        public Calculator(int memory)
+        {
+            mMemory = memory;
+        }
+
+        public int Add(int a)
+        {
+            mMemory += a;
+            return mMemory;
+        }
+
+        public int Clear()
+        {
+            mMemory = 0;
+            return mMemory;
+        }
+
+        public static int Add(int a, int b)
         {
             return a + b;
         }
--- Test/calculator-test.clj
+++ Test/calculator-test.clj
@@ -3,11 +3,18 @@
 (System.Reflection.Assembly/LoadWithPartialName "Production")
 (import (Production Calculator))
 
-(is (= 3 (Calculator/Add 1 2)))
+; test static method
+(deftest calculator-static-add
+  (is (= 3 (Calculator/Add 1 2)))
+  (is (= 7 (Calculator/Add 3 4))))
+  
+(calculator-static-add)
 
-(is (= 3 (Calculator/Add 2 2)))
+; test instance method
+(deftest calculator-add
+  (let [c (Calculator. 1)]
+    (.Add c 2)
+    (.Add c 3)
+    (is (= 10 (.Add c 4)))))
 
-;  (deftest add
-;(is (= 4 (+ 2 2)))
-;(is (= 7 (+ 3 4))))
-
+(calculator-add)
