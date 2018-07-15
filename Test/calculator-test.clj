d659c370e568db3b02f76d2c0bb462ca0914068c
Duohong <duohongopen@gmail.com>
2018-07-14 22:26:38 -0400

    added defspec

--- Test/calculator-test.clj
+++ Test/calculator-test.clj
@@ -1,10 +1,13 @@
 (assembly-load-from "Production.dll")
 
+
 (ns clojure-test-dotnet.test
   (:import (Production Calculator))
+
   (:require [clojure.test :refer :all]
             [clojure.test.check.generators :as gen]
-            [clojure.test.check.properties :as prop]))
+            [clojure.test.check.properties :as prop]
+			[clojure.test.check.clojure-test :refer [defspec]]))
 
 
 ; test static method
@@ -24,8 +27,24 @@
 
 (calculator-add)
 
+; test by gen
 (deftest calculator-add-gen
   (let [ab (gen/sample (gen/choose -1000 1000) 2)]
     (is (= (+ (first ab) (second ab)) (Calculator/Add (first ab) (second ab))))))            
 
 (calculator-add-gen)
+
+; test by spec
+(defspec calculator-add-spec 10
+  (prop/for-all [a gen/int
+		         b gen/int
+                 c gen/int
+                 d gen/int]
+    (let [cal (Calculator. a)] ; = a
+ 	  (println a b c d)
+      (is (= (+ a b) (.Add cal b)))            
+      (is (= 0       (.Clear cal)))            
+      (is (= c       (.Add cal c)))            
+      (is (= (+ c d) (.Add cal d))))))
+
+(calculator-add-spec)
