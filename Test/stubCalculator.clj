5431d316c0005ff99a91c6a7047b686604db61af
Duohong <duohongopen@gmail.com>
2018-07-29 21:02:24 -0400

    added stubCalculator

---
+++ Test/stubCalculator.clj
@@ -0,0 +1,16 @@
+(assembly-load-from "Production.dll")
+
+(ns stubCalculator
+  (:require [clojure.core])
+  (:gen-class
+    :init init
+	:main false
+    :implements [Production.ICalculator]))
+
+(defn -init []
+  [[] (ref {})]
+)
+
+(defn -Add [this a]
+  (* a a)
+)
