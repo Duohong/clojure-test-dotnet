279150a3952cc18f0584cf2385d2bb4e4e430a4d
Duohong <duohongopen@gmail.com>
2018-07-29 21:42:20 -0400

    added C# Test project

---
+++ Test/StubCalculatorTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test
+{
+    [TestClass]
+    public class StubCalculatorTest
+    {
+        [TestMethod]
+        public void TestAdd()
+        {
+            var sc = new stubCalculator();
+
+            Assert.AreEqual(4, sc.Add(2));
+            Assert.AreEqual(9, sc.Add(3));
+        }
+    }
+}
